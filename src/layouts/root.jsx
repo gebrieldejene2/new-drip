a7fd5c9d04cc6a2b7cef6257518e16ecece836ae
Gebriel Dejene <138861711+gebrieldejene2@users.noreply.github.com>
2023-12-06 02:04:02 +0000

    feat: Add mobile menu and toggle functionality
    Added MobileMenu component, openMobileMenu state, and toggleMobileMenu function

--- src/layouts/root.jsx
+++ src/layouts/root.jsx
@@ -4,15 +4,19 @@ import {
   UserIcon,
   HeartIcon,
   ShoppingBagIcon,
+  Bars3Icon,
+  MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
 
 //components
+import MobileMenu from "../components/mobile-menu";
 import Cart from "../components/cart-sidenav";
 import Favorites from "../components/favorites-sidenav";
 import Footer from "../components/footer";
 
 export default function Root() {
   const [openCat, setOpenCat] = useState("");
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
   const [openCart, setOpenCart] = useState(false);
   const [openFav, setOpenFav] = useState(false);
 
@@ -22,49 +26,65 @@ export default function Root() {
   const toggleFav = () => {
     setOpenFav(!openFav);
   };
+  const toggleMobileMenu = () => {
+    setOpenMobileMenu(!openMobileMenu);
+  };
   return (
-    <div className="flex flex-col">
-      <div className="hidden md:flex items-center text-sm w-full px-12 text-gray-700">
-        <Link className="ml-auto mr-20">Help & FAQs</Link>
-        <button className="px-3 py-1 ml-2 border-x border-gray-300">
-          <img
-            src="https://hatscripts.github.io/circle-flags/flags/ng.svg"
-            width="20"
-          ></img>
-        </button>
-      </div>
-      <header className=" bg-darkGray text-white">
-        <nav className="root-navbar flex items-center justify-center md:max-w-7xl md:h-16 md:px-10 lg:px-16 mx-auto">
-          <a href="/">
-            <h1 className="text-4xl mr-4 font-bold">Dripylux</h1>
-          </a>
-          <NavLink
-            to={"/women"}
-            onClick={() => {
-              setOpenCat("women");
-            }}
-            className="h-full flex items-center justify-center px-8 hover:bg-white hover:text-gray-700 font-bold text-sm"
-          >
-            WOMEN
-          </NavLink>
-          <NavLink
-            to={"/men"}
-            onClick={() => {
-              setOpenCat("men");
-            }}
-            className="h-full flex items-center justify-center px-10 hover:bg-white hover:text-gray-700 font-bold text-smfont-bold text-sm"
-          >
-            MEN
-          </NavLink>
-          <div className="flex-1 ml-2 mr-6">
-            <input
-              type="text"
-              name="search"
-              placeholder="Search for products..."
-              className="w-full placeholder:text-sm rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
-            />
+    <div className="flex flex-col w-full min-h-screen bg-gray-100 text-gray-700 font-sans overflow-x-hidden">
+      <header>
+        <div className="hidden md:flex items-center text-sm w-full px-12 text-gray-700">
+          <Link className="ml-auto mr-20">Help & FAQs</Link>
+          <button className="px-3 py-1 ml-2 border-x border-gray-300">
+            <img
+              src="https://hatscripts.github.io/circle-flags/flags/ng.svg"
+              width="20"
+            ></img>
+          </button>
+        </div>
+        <nav className="flex items-center w-full h-auto md:h-16 bg-darkGray text-white px-4 md:px-12 lg:px-20 py-3 md:py-0">
+          <div className="flex items-center md:hidden">
+            <button onClick={toggleMobileMenu} className="mr-4">
+              <Bars3Icon className="h-6 w-6" />
+            </button>
+            <a href="/">
+              <h1 className="font-bold">Dripylux</h1>
+            </a>
           </div>
-          <div className="flex gap-8">
+          <div className="hidden md:flex flex-1 items-center h-full">
+            <a href="/">
+              <h1 className="text-4xl mr-4 font-bold">Dripylux</h1>
+            </a>
+            <NavLink
+              to={"/women"}
+              onClick={() => {
+                setOpenCat("women");
+              }}
+              className="h-full flex items-center justify-center px-8 hover:bg-white hover:text-gray-700 font-bold text-sm"
+            >
+              WOMEN
+            </NavLink>
+            <NavLink
+              to={"/men"}
+              onClick={() => {
+                setOpenCat("men");
+              }}
+              className="h-full flex items-center justify-center px-10 hover:bg-white hover:text-gray-700 font-bold text-smfont-bold text-sm"
+            >
+              MEN
+            </NavLink>
+            <div className="flex-1 ml-2 mr-6">
+              <input
+                type="text"
+                name="search"
+                placeholder="Search for products..."
+                className="w-full placeholder:text-sm rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+              />
+            </div>
+          </div>
+          <div className="flex items-center gap-4 md:gap-8 ml-auto">
+            <button className="md:hidden">
+              <MagnifyingGlassIcon className="h-6 w-6" />
+            </button>
             <Link to="/login">
               <UserIcon className="h-6 w-6" />
             </Link>
@@ -76,7 +96,7 @@ export default function Root() {
             </button>
           </div>
         </nav>
-        <div className="bg-lightGray flex justify-center items-center">
+        <div className="hidden md:flex w-full bg-lightGray items-center justify-center">
           {openCat === "men" ? (
             <div>
               <button className="dropdown p-2">more men</button>
@@ -87,23 +107,25 @@ export default function Root() {
             </div>
           ) : null}
         </div>
-        <div className="hidden md:block bg-black text-white w-full">
-          <div className="flex justify-between items-center max-w-7xl md:px-10 lg:px-16 mx-auto">
-            <button className="border-2 border-white px-4 py-1 hover:bg-white hover:text-black">
-              WOMEN
-            </button>
-            <p className="justify-center text-center p-1">
-              NEW HERE? Get 15% off almost on everything!*
-              <br />
-              with code: <strong>HIFRIEND</strong>
-            </p>
-            <button className="border-2 border-white px-4 py-1 hover:bg-white hover:text-black">
-              MEN
-            </button>
-          </div>
+        <div className="hidden md:flex w-full bg-black text-white items-center justify-between md:px-12 lg:px-20">
+          <button className="border-2 border-white px-4 py-1 hover:bg-white hover:text-black">
+            WOMEN
+          </button>
+          <p className="justify-center text-center p-1">
+            NEW HERE? Get 15% off almost on everything!*
+            <br />
+            with code: <strong>HIFRIEND</strong>
+          </p>
+          <button className="border-2 border-white px-4 py-1 hover:bg-white hover:text-black">
+            MEN
+          </button>
         </div>
       </header>
-      <main className="flex flex-col min-h-screen">
+      <main className="flex flex-col items-center w-full min-h-screen">
+        <MobileMenu
+          openMobileMenu={openMobileMenu}
+          toggleMobileMenu={toggleMobileMenu}
+        />
         <Cart openCart={openCart} toggleCart={toggleCart} />
         <Favorites openFav={openFav} toggleFav={toggleFav} />
         <Outlet />
